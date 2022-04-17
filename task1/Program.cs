9b6ed26e0af340bd5c9cf504ccdb3691a4356a5a
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-17 20:25:44 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(100,999);
+Console.WriteLine(numberA);
+
+int numberA1 = numberA / 100;
+Console.Write(numberA1);
+
+int numberB = numberA % 10;
+Console.WriteLine (numberB);
