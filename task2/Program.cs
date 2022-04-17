e86573b3090714e666d874d290f3f7b567cb848d
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-17 21:03:49 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number>=100 && number<1000)
+{
+    Console.WriteLine("Третья цифра числа: " + number % 10);
+}
+
+if(number>=1000 && number<10000)
+{
+    int number1 = number % 100;    
+    Console.WriteLine("Третья цифра числа: " + number1 / 10);
+}
+
+if(number>=10000 && number<100000)
+{
+    int number2 = number % 1000;    
+    Console.WriteLine("Третья цифра числа: " + number2 / 100);
+}
+
+if(number<100)
+{
+    Console.WriteLine("Третья цифра отсутствует!");
+}
