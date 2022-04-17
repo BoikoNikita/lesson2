2c6237a800ea090da9d8f711159937939a125596
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-17 21:20:40 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number>7)
+{
+    Console.WriteLine("Такой день недели отсутствует!");
+}
+if(number<=5)
+{
+    Console.WriteLine("Рабочий день недели!");
+}
+if(number>=6 && number<=7)
+{
+    Console.WriteLine("Выходной!");
+}
